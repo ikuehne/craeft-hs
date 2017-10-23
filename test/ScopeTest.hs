6db778328eef0f75e63666d896a092cc5a30f9c6
ikuehne <ikuehne617@gmail.com>
2017-10-23 13:31:57 -0700

    Add test suite.

---
+++ test/ScopeTest.hs
@@ -0,0 +1,29 @@
+module Main where
+
+import Utility hiding ( SourcePos )
+import Scope
+import Text.Parsec.Pos
+
+import Control.Monad ( liftM3 )
+import Control.Monad.State
+import Control.Monad.Except
+import Test.QuickCheck
+import Test.QuickCheck.Arbitrary
+import Test.QuickCheck.Gen
+
+instance Arbitrary SourcePos where
+    arbitrary = liftM3 newPos arbitrary arbitrary arbitrary
+
+instance Arbitrary a => Arbitrary (ScopeState a) where
+    -- TODO: make this yield nested scopes.
+    arbitrary = Scope.make <$> arbitrary
+
+prop_lookupEmptyErrors :: String -> SourcePos -> Bool 
+prop_lookupEmptyErrors s p =
+    let res :: Except Error Bool
+        res = evalStateT (Scope.lookup s p) Scope.empty
+     in case runExcept res of Left _ -> True
+                              _ -> False
+
+
+main = quickCheck prop_lookupEmptyErrors
